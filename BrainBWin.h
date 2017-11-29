c75f33bcb963b66cb6cc99e9a80a71818a6ea74c
chevy71 <32552018+chevy71@users.noreply.github.com>
2017-11-29 20:50:18 +0100

    Update BrainBWin.h

--- BrainBWin.h
+++ BrainBWin.h
@@ -132,40 +132,6 @@ public:
 
     }
 
-	/*void serviceRequestFinished(QNetworkReply*) {
-
-	}*/
-
-	void sendDataToServer() {
-		CURL *curl;
-		CURLcode res;
-
-		/* In windows, this will init the winsock stuff */
-		curl_global_init(CURL_GLOBAL_ALL);
-
-		/* get a curl handle */
-		curl = curl_easy_init();
-		if (curl) {
-			/* First set the URL that is about to receive our POST. This URL can
-			just as well be a https:// URL if that is what should receive the
-			data. */
-			curl_easy_setopt(curl, CURLOPT_URL, "http://postit.example.com/moo.cgi");
-			/* Now specify the POST data */
-			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
-
-			/* Perform the request, res will get the return code */
-			res = curl_easy_perform(curl);
-			/* Check for errors */
-			if (res != CURLE_OK)
-				fprintf(stderr, "curl_easy_perform() failed: %s\n",
-					curl_easy_strerror(res));
-
-			/* always cleanup */
-			curl_easy_cleanup(curl);
-		}
-		curl_global_cleanup();
-	}
-
 	size_t static write_callback_func(char *contents, size_t size, size_t nmemb, void *userp){
 		((std::string*)userp)->append((char*)contents, size * nmemb);
 		return size * nmemb;
@@ -292,33 +258,16 @@ public:
 				+ ", \"found2lost_mean\": " + std::to_string(found2lostMean) + ", \"found2lost_var\": " + found2lostMeanString
 				+ ", \"kilobytes\": " + kilobytes
 				+ "}}";
-				
-				/*"name=\"NULL\"&gender=\"NULL\"&age=\"NULL\"&plays_regularly=\"NULL\"&is_esport_athlete=\"NULL\"&bps="
-				+ std::to_string(brainBThread->get_bps()) + "&nop=" + std::to_string(brainBThread->get_nofPaused())
-				+ "&lost_mean=" + std::to_string(lostMean) + "&lost_var=" + lostVarString
-				+ "&found_mean=" + std::to_string(foundMean) + "&found_var=" + foundVarString
-				+ "&lost2found_mean=" + std::to_string(lost2foundMean) + "&lost2found_var=" + lost2foundMeanString
-				+ "&found2lost_mean=" + std::to_string(found2lostMean) + "&found2lost_var=" + found2lostMeanString
-				+ "&kilobytes=" + kilobytes
-				+ "";*/
-
-	
-			std::cout << "DATA: " << data << std::endl;
 
 			CURL *curl;
 			CURLcode result;
 
-			/* In windows, this will init the winsock stuff */
 			curl_global_init(CURL_GLOBAL_ALL);
-
-			/* get a curl handle */
+			
 			curl = curl_easy_init();
 			char *response = NULL;
 
 			if (curl) {
-				/*First set the URL that is about to receive our POST. This URL can
-				just as well be a https:// URL if that is what should receive the
-				data. */
 				struct curl_slist *headers = NULL;
 				headers = curl_slist_append(headers, "Accept: application/json");
 				headers = curl_slist_append(headers, "Content-Type: application/json");
@@ -327,45 +276,18 @@ public:
 				
 				curl_easy_setopt(curl, CURLOPT_URL, "http://localhost/efop/handle_data.php");
 				curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-				/* Now specify the POST data */
+
 				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 
-				//curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback_func);
-				//curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
-				/* Perform the request, result will get the return code */
 				result = curl_easy_perform(curl);
-				//printf("RESPONSE: %s\n", response);
-				/* Check for errors */
-				if (res != CURLE_OK)
+	
+				/*if (res != CURLE_OK)
 					fprintf(stderr, "curl_easy_perform() failed: %s\n",
 						curl_easy_strerror(result));
-				
-				/* always cleanup */
+				*/
 				curl_easy_cleanup(curl);
 			}
 			curl_global_cleanup();
-
-			
-
-
-			/*QString js = "{\"player\": {\"name\": NULL, \"gender\": NULL, \"age\": NULL, \"plays_regularly\": NULL, \"is_esport_athlete\": NULL}, \"measurement_data\": {\"bps\": "
-				+ QString::number(brainBThread->get_bps()) + ", \"nop\": " + QString::number(brainBThread->get_nofPaused())
-				+ ", \"lost_mean\": " + QString::number(lostMean) + ", \"lost_var\": " + QString::number(lostVar)
-					+ ", \"found_mean\": " + QString::number(foundMean) + ", \"found_var\": " + QString::number(foundVar)
-					+ ", \"lost2found_mean\": " + QString::number(lost2foundMean) + ", \"lost2found_var\": " + QString::number(lost2foundVar)
-					+ ", \"found2lost_mean\": " + QString::number(found2lostMean) + ", \"found2lost_var\": " + QString::number(found2lostVar)
-					+ ", \"kilobytes\": " + QString::number(res)
-					+ "}}";
-
-			QNetworkAccessManager *networkManager = new QNetworkAccessManager(this);
-			connect(networkManager, SIGNAL(finished(QNetworkReply*)),
-				this, SLOT(serviceRequestFinished(QNetworkReply*)));
-
-			QByteArray postData = js.toUtf8();
-			QUrl serviceUrl =  QUrl(QString::fromStdString("http://localhost/efop/handle_data.php")); //TODO Ne localhost
-			QNetworkRequest request(serviceUrl);
-			request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json; charset=utf-8");
-			networkManager->post(request, postData);*/
         }
         return ret;
     }
